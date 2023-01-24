7f4cc2bc9277d1000fc6275298c32f9401f25340
Noam Haber <112407420+NoamHaber@users.noreply.github.com>
2023-01-24 17:04:17 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -26,8 +26,8 @@ function About()
                         <div className="about-item about-header">
                             !אהבתי 
                         </div>
-                        אהבתם? איזה כיף! אשמח אם תשלחו לי הודעה או סתם תיצרו קשר בעמוד הלינקדאין שלי <a style={{"color":"white","fontWeight":"bold"}} href="https://www.linkedin.com/in/noam-haber-a101521a3/" target="_blank"><FaLinkedin style={{'position':'relative','top':'2.5px','left':'1px'}}/><i>פה</i></a>. 
-                        יתפרסמו שם כמה פוסטים שמסבירים עוד על המחקר שקדם לאפליקציה ולמה בעצם יצרתי אותה. בנוסף מוזמנים לעקוב בעמוד הגיטהאב שלי <a style={{"color":"white","fontWeight":"bold"}} href="https://github.com/NoamHaber/" target="_blank"><FaGithub style={{'position':'relative','top':'2.5px','left':'1px'}}/><i>פה</i></a>
+                        אהבתם? איזה כיף! אשמח אם תשלחו לי הודעה או סתם תיצרו קשר בעמוד הלינקדאין שלי <a style={{"color":"white","fontWeight":"bold"}} href="https://www.linkedin.com/in/noam-haber-a101521a3/" target="_blank"><FaLinkedin style={{'position':'relative','top':'2.5px','left':'2.5px'}}/><i>פה</i></a>. 
+                        יתפרסמו שם כמה פוסטים שמסבירים עוד על המחקר שקדם לאפליקציה ולמה בעצם יצרתי אותה. בנוסף מוזמנים לעקוב בעמוד הגיטהאב שלי <a style={{"color":"white","fontWeight":"bold"}} href="https://github.com/NoamHaber/" target="_blank"><FaGithub style={{'position':'relative','top':'2.5px','left':'2.5px'}}/><i>פה</i></a>
                         <br />
                         <div className="about-item about-header">
                             !נסיעה נעימה
