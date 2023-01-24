91ddf87b7ecebdc5cba3a62aad5b9365a0b08526
Noam Haber <112407420+NoamHaber@users.noreply.github.com>
2023-01-24 18:25:50 +0200

    Update TeekufFooter.jsx

--- src/layout/TeekufFooter.jsx
+++ src/layout/TeekufFooter.jsx
@@ -9,7 +9,7 @@ function TeekufFooter(){
     return(    
         <div id="footer-flow" style={{'display' : (!print_mode) ? 'flex' : 'none','flexDirection':'column'}}>
             <span id="footer-text">השימוש הוא על דעת המשתמש בלבד ועל פי נהלי משרד התחבורה</span>
-            <div style={{'display':'flex','justify-content':'center'}}>
+            <div style={{'display':'flex','justify-content':'center','justify-content':'space-evenly'}}>
                 <a href="https://www.linkedin.com/in/noam-haber-a101521a3/" target="_blank" style={{'color':'blue'}} ><FaLinkedin style={{'position':'relative'}}/></a>
                 <a href="https://github.com/NoamHaber/" target="_blank" style={{'color':'red'}} ><FaGithub style={{'position':'relative'}}/></a>    
             </div>
