652968d17bfc4c6da6250ccece775e2e9688603d
Noam Haber <112407420+NoamHaber@users.noreply.github.com>
2023-01-24 18:22:24 +0200

    Update TeekufFooter.jsx

--- src/layout/TeekufFooter.jsx
+++ src/layout/TeekufFooter.jsx
@@ -7,12 +7,12 @@ function TeekufFooter(){
     const {print_mode} = useContext(BusContext);
 
     return(    
-        <div id="footer-flow" style={{'display' : (!print_mode) ? 'flex' : 'none'}}>
-            <a href="https://www.linkedin.com/in/noam-haber-a101521a3/" target="_blank" style={{'color':'blue'}} ><FaLinkedin style={{'position':'relative','top':'6px'}}/></a>
-            <span id="footer-text">
-            השימוש הוא על דעת המשתמש בלבד ועל פי נהלי משרד התחבורה
-            <a href="https://github.com/NoamHaber/" target="_blank" style={{'color':'red'}} ><FaGithub style={{'position':'relative','top':'2px','left':'4px'}}/></a>
-            </span>
+        <div id="footer-flow" style={{'display' : (!print_mode) ? 'flex' : 'none','flexDirection':'column'}}>
+            <span id="footer-text">השימוש הוא על דעת המשתמש בלבד ועל פי נהלי משרד התחבורה</span>
+            <div style={{'display':'flex','justify-content':'center'}}>
+                <a href="https://www.linkedin.com/in/noam-haber-a101521a3/" target="_blank" style={{'color':'blue'}} ><FaLinkedin style={{'position':'relative','top':'6px'}}/></a>
+                <a href="https://github.com/NoamHaber/" target="_blank" style={{'color':'red'}} ><FaGithub style={{'position':'relative','top':'2px','left':'4px'}}/></a>    
+            </div>
         </div>
     )
 }
