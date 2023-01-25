7ac528fc25c73ffd5d4f5d2eca582ba987efd412
Noam Haber <112407420+NoamHaber@users.noreply.github.com>
2023-01-25 20:46:29 +0200

    Update CreateQrCode.jsx

--- src/pages/CreateQrCode.jsx
+++ src/pages/CreateQrCode.jsx
@@ -1,8 +1,22 @@
 import TeekufForm from '../formComponents/TeekufForm'
 import StageElement from '../formComponents/StageElement';
+import { useContext } from 'react';
+import BusContext from '../Context/BusContext';
+import { useEffect } from 'react';
 
 function CreateQrCode()
 {
+    
+    const {dispatch} = useContext(BusContext);
+
+    const restartForm = () => {
+        dispatch({type:'CLEAR_INFORMATION'});
+    }
+
+    useEffect(()=>{
+        restartForm();
+    },[]);
+    
     return(
         <div>
             <StageElement />
